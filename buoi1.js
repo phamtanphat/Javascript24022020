3fdad6683bfd3eaf300e8f5741ad787fa4144ca0
phamtanphat <phatdroid94@gmail.com>
2020-02-28 03:10:27 -0800

    kieu du lieu : undefined

--- buoi1.js
+++ buoi1.js
@@ -1,6 +1,13 @@
-var a = 10
-a = "Hello"
-let b = 20
-const c = 30
-c = 40
-console.log(c)
+// 1 : Khai bao bien
+// var a = 10
+// a = "Hello"
+// let b = 20
+// const c = 30
+// c = 40
+// console.log(c)
+
+//2 : Kieu du lieu
+// Cac truong hop gap undefined
+//Th1 : Khai bao nhung khong truyen gia tri
+const a 
+console.log(a)
