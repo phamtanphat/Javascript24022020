63e22e8c02a23d5bd8571ea34880ff61fd36daa6
phamtanphat <phatdroid94@gmail.com>
2020-02-28 04:05:36 -0800

    object method

--- buoi1.js
+++ buoi1.js
@@ -13,6 +13,7 @@
 // const a 
 // console.log(a)
 //Th2 : Goi thuoc tinh khong ton tai trong object
+//Th3 : Phuong thuc khong co return hoac chi co tu khoa return 
 
 // 3 : Kieu object
 // var teo = {name : "Nguyen Van Teo",age : 10}
@@ -25,16 +26,31 @@
 
 // 5 : Toan tu
 // + - * / % ++ -- 
-var a = 5
-var b = 6
-var c = ++a - --b + ++b + a-- - b++
-
+// var a = 5
+// var b = 6
+// var c = ++a - --b + ++b + a-- - b++
 // 6 - --b + ++b + a-- - b++ ; a : 6 , b : 6
 // 6 - 5 + ++b + a-- - b++ ; a : 6 , b : 5
 // 6 - 5 + 6 + a-- - b++ ; a : 6 , b : 6
 // 6 - 5 + 6 + 6 - 6 ; a : 6 , b : 6
-
 // 7
-
 // dap an : 7
-console.log(b)
+
+// console.log(b)
+
+//7 : Function
+// function showName(){
+//     return
+// }
+// console.log(showName())
+
+// 8 : Object method
+const teo = {
+    name : "Nguyen Van Teo",
+    age : 10,
+    getName : function(){
+        return this.name
+    }
+}
+
+console.log(teo.getName())
