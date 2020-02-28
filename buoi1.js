06f69154ec0f8162f748f81aaa5c4dc51de32e3b
phamtanphat <phatdroid94@gmail.com>
2020-02-28 03:46:33 -0800

    toan tu

--- buoi1.js
+++ buoi1.js
@@ -19,6 +19,22 @@
 // console.log(teo['name'])
 
 //4 : Kieu Array
-const arrayNumber = []
-arrayNumber[0] = 10
-console.log(arrayNumber)
+// const arrayNumber = []
+// arrayNumber[0] = 10
+// console.log(arrayNumber)
+
+// 5 : Toan tu
+// + - * / % ++ -- 
+var a = 5
+var b = 6
+var c = ++a - --b + ++b + a-- - b++
+
+// 6 - --b + ++b + a-- - b++ ; a : 6 , b : 6
+// 6 - 5 + ++b + a-- - b++ ; a : 6 , b : 5
+// 6 - 5 + 6 + a-- - b++ ; a : 6 , b : 6
+// 6 - 5 + 6 + 6 - 6 ; a : 6 , b : 6
+
+// 7
+
+// dap an : 7
+console.log(b)
