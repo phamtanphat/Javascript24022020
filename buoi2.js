02dc2c02696be135ed24c8e287c4b425bb2155d6
phamtanphat <phatdroid94@gmail.com>
2020-03-02 04:20:30 -0800

    for in ,for of

--- buoi2.js
+++ buoi2.js
@@ -23,6 +23,44 @@ var a = [1, 2, 4, 8, 16];
  * 2
  * 1
  */
-for(var i = a.length - 1 ; i > -1 ; i--){
-    console.log(a[i])
+// for(var i = a.length - 1 ; i > -1 ; i--){
+//     console.log(a[i])
+// }
+
+// 11 : For in , for of
+// const numbers = [1,2,3,4,5,6,7,8,9,10]
+// const teo = {
+//     name : "Teo",
+//     age : 10
+// }
+// for(const props in teo){
+//     console.log(props)
+// }
+// for(const value of numbers){
+//     console.log(value)
+// }
+var apartment = {
+    bedroom: {
+      area: 20,
+      bed: {
+        type: 'twin-bed',
+        price: 100
+      }
+    }
+  };
+// for(const key in apartment){
+//     console.log("Key apartment: " + key)
+//     for(const keybedrom in apartment[key]){
+//         console.log("Key bedrom : " + keybedrom)
+//         for(const keybed in apartment[key][keybedrom]){
+//             console.log("Key bed : " +keybed)
+//         }
+//     }
+// }
+function getKey(object){
+    for(const key in object){
+        console.log(key)
+        if(typeof object[key] === 'object')getKey(object[key])
+    }
 }
+getKey(apartment)
