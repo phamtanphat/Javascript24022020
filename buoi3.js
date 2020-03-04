dab6e14b16abb08c70194c19e1c6a1375ca28e49
phamtanphat <phatdroid94@gmail.com>
2020-03-04 03:35:53 -0800

    btap callback

--- buoi3.js
+++ buoi3.js
@@ -21,9 +21,33 @@
 // }
 // console.log(arrayDouble([1,2,3,4,5],doubleNumber))
 
-function getName(dosth){
-    dosth()
-}
+// function getName(dosth){
+//     dosth()
+// }
+// getName(console.log)
+// 1 - 100
+// In so chan
+// In so le
+// In so chia 3 du 1
+// In so chinh phuong (can 2)
 
-getName(console.log)
+// In So chan
+// for(var i = 1 ; i <= 100 ; i++){
+//     if( i % 2 == 0) console.log(i)
+// }
+// In So le
+// for(var y = 1 ; y <= 100 ; y++){
+//     if( y % 2 == 1) console.log(y)
+// }
+// In So chia 3 du 1
+// for(var z = 1 ; z <= 100 ; z++){
+//     if( z % 3 == 1) console.log(z)
+// }
+// In So chinh phuong
+// for(var e = 1 ; e <= 100 ; e++){
+//     if(Math.sqrt(e) % 1 == 0) console.log(e)
+// }
+
+
+
 
