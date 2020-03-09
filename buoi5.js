db1ff1bdc99836139f7f64293cefe7ce16d307fd
phamtanphat <phatdroid94@gmail.com>
2020-03-09 04:51:06 -0700

    arguments

--- buoi5.js
+++ buoi5.js
@@ -9,13 +9,38 @@
 
 // Arrow function va function
 
-const arrayNames = ["Teo","Ti","Tun","Hoa"]
+// const arrayNames = ["Teo","Ti","Tun","Hoa"]
 
 // const newArraysNames = arrayNames.map(function(value){
 //     return value + " A1"
 // })
-const newArraysNames = arrayNames.map( value => {
-    value = value.toUpperCase();
-    return value + " A1"
-})
-console.log(newArraysNames)
+// const newArraysNames = arrayNames.map( value => value + " A1")
+
+// const newArraysNames = arrayNames.map( value => {
+//     value = value.toUpperCase();
+//     return value + " A1"
+// })
+// console.log(newArraysNames)
+
+// const teo = {
+//     name : "Nguyen Van Teo",
+//     age : 5,
+//     sayhi : function(){
+//         var sayHi2 = () => {
+//             console.log(this.name)
+//         }
+//         sayHi2()
+//     }
+// }
+// teo.sayhi()
+
+// Arguments
+
+function sum(){
+    console.log(typeof arguments)
+    const numbers = Array.from(arguments)
+    return numbers.reduce((sum , num) => sum + num)
+}
+  
+  console.log(sum(1,2,3,4,5,6))
+  
