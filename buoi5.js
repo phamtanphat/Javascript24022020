af0e457f82453295fabfefbbe5d3f08ec1509165
phamtanphat <phatdroid94@gmail.com>
2020-03-09 04:34:10 -0700

    arrow function

--- buoi5.js
+++ buoi5.js
@@ -1,8 +1,21 @@
-var mouse = {
-    name : 'Mickey',
-    sayhi : function() {
-      console.log('Hi, my name is' , this.name)
-    }
-  }
-const coppy = mouse.sayhi
-coppy.bind({name : "Chuot"})()
+// var mouse = {
+//     name : 'Mickey',
+//     sayhi : function() {
+//       console.log('Hi, my name is' , this.name)
+//     }
+//   }
+// const coppy = mouse.sayhi
+// coppy.bind({name : "Chuot"})()
+
+// Arrow function va function
+
+const arrayNames = ["Teo","Ti","Tun","Hoa"]
+
+// const newArraysNames = arrayNames.map(function(value){
+//     return value + " A1"
+// })
+const newArraysNames = arrayNames.map( value => {
+    value = value.toUpperCase();
+    return value + " A1"
+})
+console.log(newArraysNames)
